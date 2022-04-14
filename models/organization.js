80f4f89e2cd985dd4d342da2c9593a275036595c
jramyasri <jramyasri01@gmail.com>
2022-04-14 01:47:36 -0500

    Added Part 5

---
+++ models/organization.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+const organizationSchema = mongoose.Schema({
+name: String,
+type: String,
+noofbranches: Number
+})
+module.exports = mongoose.model("organization",
+organizationSchema)
